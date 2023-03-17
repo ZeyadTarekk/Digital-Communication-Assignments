5092e765ea589fcb82c99ee63a23455bd0f34eb4
Zeyad Tarek <zeyad.ta01@gmail.com>
2023-03-17 13:47:38 +0200

    adding the Dequantizer

--- Assignment 1/Quantization.m
+++ Assignment 1/Quantization.m
@@ -2,14 +2,23 @@
 ramp = -6:0.01:6;
 % m = 0
 q0 = UniformQuantizer(ramp, 3, 6, 0);
+dq0 = UniformDequantizer(q0, 3, 6, 0);
 figure(1);
+hold on
 plot(ramp, q0);
+plot(ramp,ramp);
+plot(ramp,dq0);
 title('m = 0');
+hold off
 % m = 1
 q1 = UniformQuantizer(ramp, 3, 6, 1);
+dq1 = UniformDequantizer(q1, 3, 6, 1);
 figure(2);
+hold on
 plot(ramp, q1);
+plot(ramp,dq1);
 title('m = 1');
+hold off
 
 % Testing the Quantizer
 in_val = [0.5, 1.2, -0.3, 2.1];
@@ -44,4 +53,15 @@ function q_ind = UniformQuantizer(in_val, n_bits, xmax, m)
         M = abs(in_val(i) - levels);
         q_ind(i) = find(M == min(M), 1);
     end
+end
+
+
+
+function deq_val = UniformDequantizer(q_ind, n_bits, xmax, m)
+
+    % Calculate the number of levels
+    numberOfLevels = 2 ^ n_bits;
+    % Get the width of each interval
+    delta = 2 * xmax / numberOfLevels;
+    deq_val = ((q_ind) * delta) + ((m + 1) * (delta / 2) - xmax);
 end
