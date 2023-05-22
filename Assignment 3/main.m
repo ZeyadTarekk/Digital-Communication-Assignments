cb3488063dee1128b3f16a092a92714ba4f135ab
Zeyad Tarek <zeyad.ta01@gmail.com>
2023-05-22 15:28:12 +0300

    Basic example

--- Assignment 3/main.m
+++ Assignment 3/main.m
@@ -1,3 +1,43 @@
+% Parameters
+size = 100;
+T = linspace(0, 1, size);
+
+% Signal s1
+s1 = ones(1, size);
+figure(1);
+plot(T, s1);
+
+% Signal s2
+s2 = zeros(1, size);
+s2(1:75) = 1;
+s2(76:end) = -1;
+figure(2);
+plot(T, s2);
+
+
+
+% Calculate the basis functions using GM_Bases function
+[phi1, phi2] = GM_Bases(s1, s2);
+
+% Plot the obtained basis functions
+figure;
+subplot(2,1,1);
+stem(phi1);
+title('Basis Function \phi_1');
+subplot(2,1,2);
+stem(phi2);
+title('Basis Function \phi_2');
+
+% Calculate the signal space representation using signal_space function
+[v1, v2] = signal_space(s1, phi1, phi2);
+
+% Plot the signal space representation
+figure;
+scatter(v1, v2);
+title('Signal Space Representation');
+xlabel('Projection onto \phi_1');
+ylabel('Projection onto \phi_2');
+
 function [phi1, phi2] = GM_Bases(s1, s2)
     % Calculate the first basis function (phi1)
     phi1 = s1 / norm(s1);
@@ -19,3 +59,4 @@ function [v1, v2] = signal_space(s, phi1, phi2)
     v1 = dot(s, phi1);
     v2 = dot(s, phi2);
 end
+
