f18f3272f50e4d3aa17ff18c8fbff7c376313b15
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-05-22 22:16:15 +0300

    Ready for submission

--- Assignment 3/main.m
+++ Assignment 3/main.m
@@ -1,8 +1,9 @@
+clc;
 clear all;
 close all;
 
 % Time axis
-N = 100;
+N = 20;
 T = linspace(0, 1, N);
 
 % Input signal s1(t)
@@ -12,8 +13,8 @@ s1 = ones(1, N);
 s2 = zeros(1, N);
 
 % Setting from 0 to 0.75 as 1, else -1 
-s2(1:75) = 1;
-s2(76:end) = -1;
+s2(1:15) = 1;
+s2(16:end) = -1;
 
 % Plot s1(t)
 figure;
@@ -81,7 +82,7 @@ grid on;
 
 % Random Samples
 % Set the random number generator seed for reproducibility
-rng('default');
+rng(0);
 
 % Number of samples
 numberOfSamples = 100;
@@ -93,8 +94,12 @@ values = [-5, 0, 10];
 L = length(values);
 
 % Get size of s1 & s2
-len_s1 = size(s1);
-len_s2 = size(s2);
+sz_s1 = size(s1);
+sz_s2 = size(s2);
+
+% Get length of s1 & s2
+len_s1 = length(s1);
+len_s2 = length(s2);
 
 for i = 1:L
     r1V1 = [];
@@ -105,16 +110,9 @@ for i = 1:L
         % Convert dB to linear scale
         val = 10^(values(i)/10);
 
-        % Calculate the standard deviation
-        sigma = 1 / sqrt(val);
-
-        % Noise Signals w1 & w2 to be added to s1 & s2
-        w1 = sigma * randn(len_s1);
-        w2 = sigma * randn(len_s2);
-
-        % Generate samples of r1(t) and r2(t) using s1(t) and s2(t) & noise
-        r1 = s1 + w1;
-        r2 = s2 + w2;
+        % Add noise to the original signal
+        r1 = awgn(s1, Es1/val);
+        r2 = awgn(s2, Es2/val);
 
         % Calculate the signal points of the input using signal_space function
         [r1_v1, r1_v2] = signal_space(r1, phi1, phi2);
@@ -126,7 +124,7 @@ for i = 1:L
         r2V1(end + 1) = r2_v1;
         r2V2(end + 1) = r2_v2;
     end
-    
+    disp(values(i));
     % Plot the signal points of generated samples of r1 & r2
     figure;
     % Plot r1 samples
@@ -136,10 +134,10 @@ for i = 1:L
     scatter(r2V1, r2V2, 'DisplayName', 'r2');
     hold on
     % Plot the input signal s1(t)
-    scatter(s1_v1, s1_v2, 110, 'filled', 'MarkerFaceColor', 'red', 'DisplayName', 's1');
+    scatter(s1_v1, s1_v2, 100, 'filled', 'MarkerFaceColor', 'red', 'DisplayName', 's1');
     hold on
     % Plot the input signal s2(t)
-    scatter(s2_v1, s2_v2, 110, 'filled', 'MarkerFaceColor', 'blue', 'DisplayName', 's2');
+    scatter(s2_v1, s2_v2, 100, 'filled', 'MarkerFaceColor', 'blue', 'DisplayName', 's2');
     % Title/xLabel/yLabel
     title(sprintf('Signal Points (sigma^2 = %ddB)', values(i)));
     xlabel('Projection onto \phi_1');
@@ -149,9 +147,15 @@ for i = 1:L
 end
 
 function [phi1, phi2] = GM_Bases(s1, s2)
-    % sq_s1 = s1.^2;
-    % energy = sum(sq_s1);
-    % disp(energy);
+    % Getting each signal length
+    len_s1 = length(s1);
+    len_s2 = length(s2);
+    
+    % Signal Energy
+    sq_s1 = s1.^2;
+    energy = sum(sq_s1);
+    disp(energy);
+    
     % Calculate the first basis function (phi1)
     phi1 = s1 / norm(s1);
 
@@ -164,6 +168,10 @@ function [phi1, phi2] = GM_Bases(s1, s2)
         % s2 is linearly dependent on s1, so phi2 is a zero vector
         phi2 = zeros(size(s2));
     end
+    
+    % Multiplying by the square root of signal length
+    phi1 = phi1 * sqrt(len_s1);
+    phi2 = phi2 * sqrt(len_s2);
 end
 
 function [v1, v2] = signal_space(s, phi1, phi2)
@@ -175,7 +183,13 @@ function [v1, v2] = signal_space(s, phi1, phi2)
     phi1 = phi1 / sqrt(phi1_len);
     phi2 = phi2 / sqrt(phi2_len);
     
+    % Calculating the dot product between the signal and phi just like the
+    % lecture
+    dotProduct_1 = dot(s, phi1);
+    dotProduct_2 = dot(s, phi2);
+    
     % Calculate the projections (correlations) of s over phi1 and phi2
-    v1 = dot(s, phi1);
-    v2 = dot(s, phi2);
+    % Then divide by the square root of each phi
+    v1 = dotProduct_1 / sqrt(phi1_len);
+    v2 = dotProduct_2 / sqrt(phi2_len);
 end
